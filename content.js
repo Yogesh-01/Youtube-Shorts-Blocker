9bd91fc8ffb1d7ed9310bb8fc8aeb633fa77c4a9
Yogesh Kumar <yogeshkumar7689@gmail.com>
2022-03-01 21:37:24 +0530

    final version

--- content.js
+++ content.js
@@ -45,18 +45,11 @@ window.onload=function(){
             }
 
                 let t1 = time_str[time_str.indexOf(':')-2]
-               // console.log(t1)
                 let t2 = time_str[time_str.indexOf(':')-1]
-                //console.log(t2)
                 let t3 = time_str[time_str.indexOf(':')+1]
-                //console.log(t3)
                 let t4 = time_str[time_str.indexOf(':')+2]
-                //console.log(t4)
                 let t5 = time_str[time_str.indexOf(':')+4]
-               // console.log(t5)
                 let t6 = time_str[time_str.indexOf(':')+5]
-               // console.log(t6)
-               //console.log(time_str.trim())
                 
                 let h=0
                 let m=0
@@ -65,7 +58,6 @@ window.onload=function(){
                 // if t6 number, then string is hours
                 if(isNum(t5)){
                     // console.log("hours present")
-                    //  console.log(Number(t1+t2))
                      h=Number(t1+t2)
                      m=Number(t3+t4)
                      s=Number(t5+t6)
@@ -76,7 +68,7 @@ window.onload=function(){
                      s=Number(t3+t4)
                 }
 
-           // console.log('h - '+h+' m- '+m+' s- '+s)
+            // console.log('h - '+h+' m- '+m+' s- '+s)
     
          
 
@@ -88,12 +80,10 @@ window.onload=function(){
             } 
            }
            
-          // console.log(curr.querySelector('span#text'))
 
         }
-
      
-      }, 500);
+}, 500);
    
 
 
@@ -111,7 +101,7 @@ window.onload=function(){
         if(exec==1)curr_home.style.border = "1px solid rgba(196, 196, 196, 0.4)";
 
         let curr_vid_dur_home = curr_home.querySelector('span#text')
-        //if(curr_vid_dur_home!=null)curr_vid_dur_home.style.border = "1px solid yellow";
+        
         if(curr_vid_dur_home!=null) var time_str_home = curr_vid_dur_home.textContent
         if(time_str_home==null) continue;                                                           ///
         if(time_str_home.trim() == "SHORTS"){
@@ -125,18 +115,11 @@ window.onload=function(){
         }
 
             let t1_home = time_str_home[time_str_home.indexOf(':')-2]
-           // console.log(t1)
             let t2_home = time_str_home[time_str_home.indexOf(':')-1]
-            //console.log(t2)
             let t3_home = time_str_home[time_str_home.indexOf(':')+1]
-            //console.log(t3)
             let t4_home = time_str_home[time_str_home.indexOf(':')+2]
-            //console.log(t4)
             let t5_home = time_str_home[time_str_home.indexOf(':')+4]
-           // console.log(t5)
             let t6_home = time_str_home[time_str_home.indexOf(':')+5]
-           // console.log(t6)
-          //console.log(time_str_home.trim())
             
             let h_home=0
             let m_home=0
@@ -144,8 +127,7 @@ window.onload=function(){
 
             // if t6 number, then string is hours
             if(isNum(t5_home)){
-                 //console.log("hours present")
-                //console.log(Number(t1_home+t2_home))
+
                  h_home=Number(t1_home+t2_home)
                  m_home=Number(t3_home+t4_home)
                  s_home=Number(t5_home+t6_home)
@@ -158,8 +140,6 @@ window.onload=function(){
 
         //console.log('h - '+h_home+' m- '+m_home+' s- '+s_home)
 
-       //console.log(vid_list.length)
-
        if((m_home<=0 && h_home==0)||(m_home==1 && h_home==0 && s_home<=40)){
         if(exec==1){
             curr_home.style.border='1px solid red'
@@ -168,12 +148,10 @@ window.onload=function(){
         } 
        }
        
-     //  console.log(curr_home.querySelector('span#text'))
 
     }
-
  
-  }, 500);
+}, 500);
 
                                                 /*** NEWS AND OTHER AT HOME ***/
 
@@ -189,9 +167,9 @@ var intervalId_home = window.setInterval(function(){  // main repeating function
         if(exec==1) curr_news.style.border = "1px solid rgba(196, 196, 196, 0.4)";
 
         let curr_vid_dur_news = curr_news.querySelector('span#text')
-        //if(curr_vid_dur_news!=null)curr_vid_dur_news.style.border = "1px solid yellow";
+
         if(curr_vid_dur_news!=null) var time_str_news = curr_vid_dur_news.textContent
-        if(time_str_news==null) continue;                                                           ///
+        if(time_str_news==null) continue;                                                        
         if(time_str_news.trim() == "SHORTS"){
             if(exec==1){
                 curr_news.style.border='3px solid red'
@@ -201,20 +179,12 @@ var intervalId_home = window.setInterval(function(){  // main repeating function
            continue
            
         }
-
             let t1_news = time_str_news[time_str_news.indexOf(':')-2]
-           // console.log(t1)
             let t2_news = time_str_news[time_str_news.indexOf(':')-1]
-            //console.log(t2)
             let t3_news = time_str_news[time_str_news.indexOf(':')+1]
-            //console.log(t3)
             let t4_news = time_str_news[time_str_news.indexOf(':')+2]
-            //console.log(t4)
             let t5_news = time_str_news[time_str_news.indexOf(':')+4]
-           // console.log(t5)
             let t6_news = time_str_news[time_str_news.indexOf(':')+5]
-           // console.log(t6)
-         //  console.log(time_str_news.trim())
             
             let h_news=0
             let m_news=0
@@ -222,8 +192,7 @@ var intervalId_home = window.setInterval(function(){  // main repeating function
 
             // if t6 number, then string is hours
             if(isNum(t5_news)){
-                // console.log("hours present")
-                // console.log(Number(t1_news+t2_news))
+
                  h_news=Number(t1_news+t2_news)
                  m_news=Number(t3_news+t4_news)
                  s_news=Number(t5_news+t6_news)
@@ -244,12 +213,11 @@ var intervalId_home = window.setInterval(function(){  // main repeating function
         } 
        }
        
-    //   console.log(curr_news.querySelector('span#text'))
 
     }
 
  
-  }, 500);
+}, 500);
 
 
                                                         /** Subscription  page  **/
@@ -265,7 +233,7 @@ var intervalId_home = window.setInterval(function(){  // main repeating function
         if(exec==1) curr_subs.style.border = "1px solid rgba(196, 196, 196, 0.4)";
 
         let curr_vid_dur_subs = curr_subs.querySelector('span#text')
-       // curr_vid_dur_subs.style.border = "1px solid yellow";
+
         if(curr_vid_dur_subs==null)continue
         var time_str_subs = curr_vid_dur_subs.textContent
         if(time_str_subs==null) continue
@@ -280,18 +248,11 @@ var intervalId_home = window.setInterval(function(){  // main repeating function
         }
 
             let t1_subs = time_str_subs[time_str_subs.indexOf(':')-2]
-           // console.log(t1)
             let t2_subs = time_str_subs[time_str_subs.indexOf(':')-1]
-            //console.log(t2)
             let t3_subs = time_str_subs[time_str_subs.indexOf(':')+1]
-            //console.log(t3)
             let t4_subs = time_str_subs[time_str_subs.indexOf(':')+2]
-            //console.log(t4)
             let t5_subs = time_str_subs[time_str_subs.indexOf(':')+4]
-           // console.log(t5)
             let t6_subs = time_str_subs[time_str_subs.indexOf(':')+5]
-           // console.log(t6)
-         //  console.log(time_str_subs.trim())
             
             let h_subs=0
             let m_subs=0
@@ -299,8 +260,7 @@ var intervalId_home = window.setInterval(function(){  // main repeating function
 
             // if t6 number, then string is hours
             if(isNum(t5_subs)){
-                 //console.log("hours present")
-                 //console.log(Number(t1_subs+t2_subs))
+
                  h_subs=Number(t1_subs+t2_subs)
                  m_subs=Number(t3_subs+t4_subs)
                  s_subs=Number(t5_subs+t6_subs)
@@ -322,12 +282,11 @@ var intervalId_home = window.setInterval(function(){  // main repeating function
         } 
        }
        
-      // console.log(curr_subs.querySelector('span#text'))
 
     }
 
 
-  }, 500);
+}, 500);
 
 
 
@@ -347,10 +306,10 @@ var intervalId_home = window.setInterval(function(){  // main repeating function
 
         let curr_vid_dur_sidebar = curr_sidebar.querySelector('span#text')
         if(curr_vid_dur_sidebar==null)continue
-        //if(curr_vid_dur_sidebar!=null)curr_vid_dur_sidebar.style.border = "1px solid yellow";
+
         if(curr_vid_dur_sidebar!=null) var time_str_sidebar = curr_vid_dur_sidebar.textContent
-        if(time_str_sidebar==null) continue                                         //
-        if(time_str_sidebar.trim() == "SHORTS"){                          //
+        if(time_str_sidebar==null) continue                                         
+        if(time_str_sidebar.trim() == "SHORTS"){                          
             if(exec==1){
                 curr_sidebar.style.border='3px solid red'
                 curr_sidebar.remove()
@@ -362,18 +321,11 @@ var intervalId_home = window.setInterval(function(){  // main repeating function
         }
 
             let t1_sidebar = time_str_sidebar[time_str_sidebar.indexOf(':')-2]
-           // console.log(t1)
             let t2_sidebar = time_str_sidebar[time_str_sidebar.indexOf(':')-1]
-            //console.log(t2)
             let t3_sidebar = time_str_sidebar[time_str_sidebar.indexOf(':')+1]
-            //console.log(t3)
             let t4_sidebar = time_str_sidebar[time_str_sidebar.indexOf(':')+2]
-            //console.log(t4)
             let t5_sidebar = time_str_sidebar[time_str_sidebar.indexOf(':')+4]
-           // console.log(t5)
             let t6_sidebar = time_str_sidebar[time_str_sidebar.indexOf(':')+5]
-           // console.log(t6)
-           //console.log(time_str_sidebar.trim())
             
             let h_sidebar=0
             let m_sidebar=0
@@ -381,8 +333,7 @@ var intervalId_home = window.setInterval(function(){  // main repeating function
 
             // if t6 number, then string is hours
             if(isNum(t5_sidebar)){
-                 //console.log("hours present")
-                 //console.log(Number(t1_sidebar+t2_sidebar))
+
                  h_sidebar=Number(t1_sidebar+t2_sidebar)
                  m_sidebar=Number(t3_sidebar+t4_sidebar)
                  s_sidebar=Number(t5_sidebar+t6_sidebar)
@@ -395,8 +346,6 @@ var intervalId_home = window.setInterval(function(){  // main repeating function
 
        // console.log('h - '+h_sidebar+' m- '+m_sidebar+' s- '+s_sidebar)
 
-       //console.log(vid_list.length)
-
        if((m_sidebar<=0 && h_sidebar==0)||(m_sidebar==1 && h_sidebar==0 && s_sidebar<=40)){
         if(exec==1){
             curr_sidebar.style.border='1px solid red'
@@ -405,12 +354,11 @@ var intervalId_home = window.setInterval(function(){  // main repeating function
         } 
        }
        
-    //   console.log(curr_sidebar.querySelector('span#text'))
 
     }
 
  
-  }, 500);
+}, 500);
 
 
                             /***  SHORTS PLAYING OVERLAY***/
--- popup_js.js
+++ popup_js.js
@@ -4,24 +4,12 @@ function(){checkk();}
 );
 
 
-//var session_;
-//chrome.storage.sync.get(['click_status'],function(result){session_ = result.click_status})
-//if(session_==null)chrome.storage.sync.set({click_status: -1})
-
 chrome.browserAction.onClicked.addListener(iconClicked)
 function iconClicked(){
-    // if(chrome.storage.sync.get(['click_status'])==1)document.querySelector('#togBtn').checked=true
-    // if(chrome.storage.sync.get(['click_status'])==0)document.querySelector('#togBtn').checked=false
-
     var session_click=localStorage.getItem("click_stat")
     isChecked= document.querySelector('#togBtn').checked
     checkk()
-
-    
-
 }
-
-// local storage dies after popup closes, so use other storage
 
 let msg= {
     text: ""
@@ -31,39 +19,25 @@ var isChecked = false
 function checkk(){
     var session_click=localStorage.getItem("click_stat")
     isChecked= document.querySelector('#togBtn').checked
-//     if(session_click==-1 && isChecked==true){
-//         msg.text="hii"
-//         pingContentJs()
-//         //document.querySelector('#togBtn').checked=truee 1
-//         console.log("clickeddddd")
-//         localStorage.setItem("click_stat", 1)
-//     } 
-//    else if(isChecked==false && session_click==1){
-//         msg.text="bye"
-//         pingContentJs()
-//         //document.querySelector('#togBtn').checked=false  2
-//         console.log('not clicked')
-//         localStorage.setItem("click_stat", 0)
-//     }
- 
+
     if(isChecked==true ){
         msg.text="hii"
         pingContentJs()
-        //document.querySelector('#togBtn').checked=true  3
+        
         console.log('clickeddddd')
         localStorage.setItem("click_stat", 1)
     }
     else if(session_click==1 || isChecked==false){
         msg.text="bye"
         pingContentJs()
-        //document.querySelector('#togBtn').checked=false 4
+        
         console.log("not clicked")
         localStorage.setItem("click_stat", 0)
     } 
     
 
 }
-setInterval(change_toggle_btn_by_session, 100)
+setInterval(change_toggle_btn_by_session, 100) //  inefficient, but not gonna work on that 
 
 function change_toggle_btn_by_session(){
     console.log(localStorage.getItem("click_stat"))
